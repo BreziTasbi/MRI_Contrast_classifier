9021e801439056b1849fa9dbef44865f09c28796
Baptiste TAVERNE <baptiste.taverne@polytechnique.edu>
2024-04-11 12:06:08 -0400

    Ready2Train_1

--- train.py
+++ train.py
@@ -1,11 +1,33 @@
 from torch.utils.data import DataLoader
 from data_manager import Dataset_2D, find_T1w_T2w_paths, dataset_splitter, Dataset_2D, paths_to_Dataset
-from contrast_classifier_Networks import ResNet18SingleChannel, FCN
+from contrast_classifier_Networks import ResNet18SingleChannel
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import numpy as np
 from sklearn.metrics import f1_score
+import argparse
+
+
+### Retrieve the arguments
+# Create the parser
+parser = argparse.ArgumentParser(description='Train and evaluate the model.')
+
+# Add the arguments
+parser.add_argument('--evaluate', type=bool, default=True,
+                    help='a boolean for the evaluation mode, True by Default')
+parser.add_argument('--model_path', type=str, default='none',
+                    help='the path to the model, Random weights by Default')
+parser.add_argument('--model_output', type=str, default='checkpoints//model.pth',
+                    help='the path to the output model, checkpoints//model.pth by Default')
+
+# Parse the arguments
+args = parser.parse_args()
+
+# Access the arguments
+evaluate = args.evaluate
+model_path = args.model_path
+model_output = args.model_output
 
 # Define the training loop
 def training_one_epoch(model):
@@ -15,13 +37,10 @@ def training_one_epoch(model):
     np.random.shuffle(queue_line) 
     index=0
     for i in queue_line:
-        if index%200 == 0:
-            print(index)
         image, label = train_dataset[i]
         image, label = image.to(device), label.to(device)
         optimizer.zero_grad()
         outputs = model(image)
-        print(outputs.shape, label.shape)
         loss = criterion(outputs, label.float())
         loss.backward()
         optimizer.step()
@@ -48,8 +67,10 @@ val_dataset = paths_to_Dataset(pd_val_data)
 # Define the device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-#model = ResNet18SingleChannel(num_classes=2).to(device)
-model = FCN().to(device)
+model = ResNet18SingleChannel(num_classes=2).to(device)
+if model_path != 'none':
+    model.load_state_dict(torch.load(model_path))
+    print("Model loaded")
 criterion = nn.BCELoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
@@ -61,16 +82,14 @@ for epoch in range(num_epochs):
     model, train_loss = training_one_epoch(model)
     print(f"Epoch {epoch + 1} training loss: {train_loss}")
     if (epoch + 1) % 10 == 0:
-        torch.save(model.state_dict(), f"model_{epoch + 1}.pth")
+        torch.save(model.state_dict(), f"checkpoints//model_save_epoch_{epoch + 1}.pth")
 #save model
-torch.save(model.state_dict(), "model.pth")
+torch.save(model.state_dict(), model_output)
 
 #### Evaluate the model
-evaluate = False
 
 if evaluate:
     # Assess accuracy and F1 score on the validation set
-    model.eval()
     val_predictions = []
     val_labels = []
     for i in range(len(val_dataset)):
